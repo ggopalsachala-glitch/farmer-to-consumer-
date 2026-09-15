f5e81f2f7468885d358e7a7624f325c6575c7339
ggopalsachala-glitch <ggopalsachala@gmail.com>
2026-09-15 21:04:48 +0530

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,13 @@
 
+# Farmer to Consumer
+
+A website for connecting farmers directly with consumers.
+
+## Features
+- Fresh green vegetables
+- Farmer to consumer direct transaction
+- Product information
+- Easy purchasing
+
+## Project
+ 
